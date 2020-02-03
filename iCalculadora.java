be9a2dea5e295decd51333235c5585b5b3e93e7c
lauratamath <laurylevi@hotmail.com>
2020-02-03 10:54:20 -0600

    interfaz

---
+++ iCalculadora.java
@@ -0,0 +1,9 @@
+//Laura Tamath	19365
+//Walter
+
+public interface iCalculadora{
+	public int sumar(int operando1,int operando2);
+	public int restar(int operando1,int operando2);
+	public int dividir(int operando1,int operando2);
+	public int multiplicar(int operando1,int operando2); 
+}
