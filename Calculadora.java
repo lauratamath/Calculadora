0f0fcaeec7fd327f748b81c843c94bf0c805b306
lauratamath <laurylevi@hotmail.com>
2020-02-04 13:59:42 -0600

    clase calcu

---
+++ Calculadora.java
@@ -0,0 +1,36 @@
+//Laura Tamath 19365
+//Walter 
+
+public class Calculadora implements iCalculadora{
+	int operando1; //Es el primer número al que se operará
+	int operando2; //Segundo número a operar
+
+	//Metodo de suma
+	public int sumar(int operando1, int operando2){
+		//Pre: hay dos operandos 
+		return operando1 + operando2;
+		//Post: los operandos se suman
+	}
+
+	//metodo de resta
+	public int resta(int operando1, int operando2){
+		//Pre: hay dos operandos
+		return operando1 - operando2;
+		//Post: los operandos se restan
+	}
+
+	//metodo de multiplicacion
+	public int multiplicar(int operando1, int operando2){
+		//Pre: hay dos operandos
+		return operando1 * operando2;
+		//Post: los operandos se multiplican
+	}
+
+	//metodo de resta
+	public int dividir(int operando1, int operando2){
+		//Pre: hay dos operandos
+		return operando1/operando2;
+		//Post: los operandos se dividen
+	}
+	
+}
