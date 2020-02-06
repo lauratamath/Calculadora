0cb21db23199583fe215c38cd38dcdd75d562137
lauratamath <laurylevi@hotmail.com>
2020-02-05 20:12:16 -0600

    Parte del main

--- Calculadora.java
+++ Calculadora.java
@@ -1,9 +1,19 @@
-//Laura Tamath 19365
-//Walter 
+/**
+ * @author Walter Saldaña
+ * @author Laura Tamath
+ * 
+ * Intefaz que posee las operaciones que esta calculadora puede ejecutar
+ * Se vincula al último nodo de la colección. 
+ */
 
 public class Calculadora implements iCalculadora{
-	int operando1; //Es el primer número al que se operará
-	int operando2; //Segundo número a operar
+	//int operando1; //Es el primer número al que se operará
+	//int operando2; //Segundo número a operar
+
+	/**
+	 * @param operando1		Segundo numero a sumar/restar/multiplicar/dividir
+	 * @param operando2     Primer numero a sumar/restar/multiplicar/dividir
+	*/
 
 	//Metodo de suma
 	public int sumar(int operando1, int operando2){
--- Main.java
+++ Main.java
@@ -11,9 +11,31 @@
  * y se retorna el resultado numérico.
  */
 
+//Importación de clases
+import java.util.Scanner; //Leer la entrada de texto
+import java.io.File; //Representa un archivo 
+import java.io.FileNotFoundException; //Intento fallido al abrir un archivo
+
+
 public class Main{
     public static void main(String[] args){
-        PostfixCalculator pc = new PostfixCalculator("6 2 3 + *");
-        System.out.println(String.valueOf(pc.calcular()));
+    	iCalculadora calcu = new Calculdora();
+    	Stack<Integer> vector = new Vector<Integer>;
+    	Scanner sc = new Scanner(System.in); 
+
+    	String [] operaciones; //Operaciones establecidas en el txt
+        String datos = "" //Lo que se encuentre en el txt se establecera en esta.
+        int operando1, operando2, num;
+        int result=0;
+
+        //Apertura del archivo con defensa
+        try{
+        	Scanner ver = new Scanner(new File("datos.txt"));//Leer el archivo
+        	datos += ver.nextLine();
+        	ver.close();
+        } catch(FileNotFoundException e){//Si el archivo txt no se encuentra
+        	e.printStackTrace();
+        }operaciones= datos.split(" ");
+
     }
 }
